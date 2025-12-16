9c1bcbcf2d5c19924d51c0ea5407458a6a4a5845
dyuzha <matvey10d@gmail.com>
2025-12-16 14:31:18 +0300

    Исправил баги.

--- nginx/scripts/entrypoint.sh
+++ nginx/scripts/entrypoint.sh
@@ -5,6 +5,7 @@ VARS="$(sed 's/.*/${&}/' /etc/nginx/vars.list | tr '\n' ' ')"
 
 envsubst "$VARS" \
   < /etc/nginx/nginx.conf.template \
-  > /etc/nginx/nginx.conf
+  > /usr/local/openresty/nginx/conf/nginx.conf
+  # > /etc/nginx/nginx.conf
 
 exec openresty -g 'daemon off;'
